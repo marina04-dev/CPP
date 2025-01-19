376b430b1f48d94fb3e1b9d349dff82e80272d56
marina04-dev <marinachatzeephraimidou@gmail.com>
2025-01-19 20:00:21 +0200

    Create video23_passing-arguments-by-pointer.cpp

---
+++ video23_passing-arguments-by-pointer.cpp
@@ -0,0 +1,19 @@
+// Passing an argument by pointer
+#include <iostream>
+using namespace std;
+
+void change(int *pA);
+
+int main() {
+    int a = 1;
+    cout<<"main:a="<<a<<endl;
+    change(&a);
+    cout<<"main:a="<<a<<endl;
+    
+    return 0;
+}
+
+void change(int *pA) {
+    *pA=2;
+    cout<<"change: *pA="<<*pA<<endl;
+}
