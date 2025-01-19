b7c7587d5c59594e4d65bbef187c76a815c9dac7
marina04-dev <marinachatzeephraimidou@gmail.com>
2025-01-19 19:45:21 +0200

    Create video22_references-example.cpp

---
+++ video22_references-example.cpp
@@ -0,0 +1,16 @@
+// Referencses example
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 3;
+    int &r = x;
+    cout<<"x="<<x<<" r="<<r<<endl;
+    x = 4;
+    cout<<"x="<<x<<" r="<<r<<endl;
+    r = 5;
+    cout<<"x="<<x<<" r="<<r<<endl;
+    
+    return 0;
+    
+}
