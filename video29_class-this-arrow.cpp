ee2309619df8e08c09951ad49f2748f54a10454d
marina04-dev <marinachatzeephraimidou@gmail.com>
2025-01-29 03:07:56 -0800

    Create video29_class-this-arrow.cpp

---
+++ video29_class-this-arrow.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+class point {
+    public:
+        point(int x, int y);
+        void print();
+    private:
+        int x;
+        int y;
+};
+
+int main() {
+    point ob(3,4);
+    ob.print();
+    return 0;
+}
+
+point::point(int x, int y) {
+    this->x=x;
+    this->y=y;
+}
+
+void point::print() {
+    cout<<"("<<x<<","<<y<<")";
+}
