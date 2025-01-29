39d2da9f85ab6379c4549e63cea2eea299f19d49
marina04-dev <marinachatzeephraimidou@gmail.com>
2025-01-29 03:44:25 -0800

    Create video130_dynamic-array-class.cpp

---
+++ video130_dynamic-array-class.cpp
@@ -0,0 +1,43 @@
+/* Dynamic Array that consists of objects - Setters-Getters */
+#include <iostream>
+using namespace std;
+
+
+class dummy {
+    public:
+        dummy();
+        void set_x(int in_x);
+        int get_x() const;
+    private:
+        int x;
+};
+
+int main() {
+    dummy *array;
+    int n =3;
+    
+    array = new dummy [n];
+    if (!array) cout<<"Error Allocating Memory!";
+    
+    for (int i=0; i<n; i++) {
+        array[i].set_x(i*i);
+    }
+    for (int i=0; i<n; i++) {
+        cout<<array[i].get_x()<<" ";
+    }
+    delete [] array;
+    
+    return 0;
+}
+
+dummy::dummy() {
+    x=0;
+}
+
+void dummy::set_x(int in_x) {
+    x=in_x;
+}
+
+int dummy::get_x() const {
+    return x;
+}
