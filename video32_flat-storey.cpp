14f01dabe0d9d5be4133253faf10bf20c308e502
marina04-dev <marinachatzeephraimidou@gmail.com>
2025-02-03 04:46:17 -0800

    Update video32_flat-storey.cpp

--- video32_flat-storey.cpp
+++ video32_flat-storey.cpp
@@ -21,9 +21,26 @@ class storey {
         flat flats[2];
 };
 
+class building {
+    public:
+        building();
+        ~building();
+        void set_people(int in_storey, int in_flat, int in_people);
+        int get_people() const;
+    private:
+        storey storeys[2];
+};
+
 int main()
 {
-    std::cout<<"Hello World";
+    building b;
+    b.set_people(0,0,3);
+    b.set_people(0,1,2);
+    b.set_people(1,0,2);
+    b.set_people(1,1,1);
+    cout<<endl;
+    cout<<"People in building: "<<b.get_people();
+    cout<<endl<<endl;
 
     return 0;
 }
@@ -60,4 +77,20 @@ void storey::set_people(int in_flat, int in_people) {
 
 int storey::get_people() const {
     return flats[0].get_people()+flats[1].get_people();
+}
+
+building::building() {
+    cout<<"Constructing building..."<<endl;
+}
+
+building::~building() {
+    cout<<"Destructing building..."<<endl;
+}
+
+void building::set_people(int in_storey, int in_flat, int in_people) {
+    storeys[in_storey].set_people(in_flat, in_people);
+}
+
+int building::get_people() const {
+    return storeys[0].get_people()+storeys[1].get_people();
 }
