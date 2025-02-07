27f0ab221f1fe9383b995ea1b7c72203996fb6f1
marina04-dev <marinachatzeephraimidou@gmail.com>
2025-02-07 02:30:19 -0800

    Create video51_friend-class-method.cpp

---
+++ video51_friend-class-method.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+class man;
+
+class state {
+    public:
+        void withdraw(man &ob);
+};
+
+class man {
+    public:
+        friend void state::withdraw(man &ob);
+        man(int in_money);
+        int get_money() const;
+    private:
+        int money;
+};
+
+int main() {
+    man Papadakis(1500);
+    state Greece;
+    cout<<"Papadakis Money="<<Papadakis.get_money()<<endl;
+    Greece.withdraw(Papadakis);
+    cout<<"Papadakis Money="<<Papadakis.get_money()<<endl;
+    return 0;
+}
+
+void state::withdraw(man &ob) {
+    ob.money = 0;
+}
+
+man::man(int in_money) {
+    money=in_money;
+}
+
+int man::get_money() const {
+    return money;
+}
