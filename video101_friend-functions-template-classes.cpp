e01153c38ff79934185d7070335e46406d379ea2
marina04-dev <marinachatzeephraimidou@gmail.com>
2025-02-20 00:37:05 -0800

    Create video101_friend-functions-template-classes.cpp

---
+++ video101_friend-functions-template-classes.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+#define STACK_SIZE 100
+
+template <typename T>
+class Stack {
+    public:
+        Stack();
+        bool empty();
+        bool full();
+        bool push(T elem);
+        bool pop(T *elem);
+        template <typename U>friend ostream &operator<<(ostream &left, const Stack<U> &right);
+    private:
+        T array[STACK_SIZE];
+        int top;
+};
+
+int main() {
+    /* Integer Stack */
+    Stack<int> st;
+    int temp;
+    
+    st.push(2);
+    st.push(5);
+    
+    st.pop(&temp);
+    cout<<temp<<" ";
+    st.pop(&temp);
+    cout<<temp<<" ";
+    cout<<endl<<st;
+    
+    
+    /* String Stack */
+    Stack<string> st2;
+    string temp2;
+    
+    st2.push("Hello");
+    st2.push("World");
+    
+    st2.pop(&temp2);
+    cout<<temp2<<" ";
+    st2.pop(&temp2);
+    cout<<temp2<<" ";
+    cout<<endl<<st2;
+    return 0;
+}
+
+template <typename T>
+Stack<T>::Stack() {
+    top = 1;
+}
+
+template <typename T>
+bool Stack<T>::empty() {
+    return top == -1;
+}
+
+template <typename T>
+bool Stack<T>::full() {
+    return top == STACK_SIZE-1;
+}
+
+template <typename T>
+bool Stack<T>::push(T elem) {
+    if (full()) {
+        return false;
+    }
+    top++;
+    array[top]=elem;
+    return true;
+}
+
+template <typename T>
+bool Stack<T>::pop(T *elem) {
+    if (empty()) {
+        return false;
+    }
+    *elem = array[top];
+    top--;
+    return true;
+}
+
+template <typename T>
+ostream &operator<<(ostream &left, const Stack<T> &right) {
+    for (int i=0; i<=right.top; i++) {
+        left<<right.array[i]<<" ";
+    }
+    return left;
+}
