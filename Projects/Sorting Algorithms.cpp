4631d3bd37a55924d3242e2859f0ec745e9e80e1
marina04-dev <marinachatzeephraimidou@gmail.com>
2025-04-03 01:10:38 -0700

    Create Sorting Algorithms.cpp

---
+++ Projects/Sorting Algorithms.cpp
@@ -0,0 +1,164 @@
+#include <iostream>
+#include <utility>
+using namespace std;
+
+void printArray(int arr[], int size);
+void bubbleSort(int arr[], int size);
+void insertionSort(int arr[], int size);
+void selectionSort(int arr[], int size);
+void merge(int arr[], int left, int mid, int right);
+void mergeSort(int arr[], int left, int right);
+int partition(int arr[], int low, int high);
+void quickSort(int arr[], int low, int high);
+void heapify(int arr[], int size, int i);
+void heapSort(int arr[], int size);
+
+int main() {
+    return 0;
+}
+
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; ++i) {
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
+
+
+void bubbleSort(int arr[], int size) {
+    for (int i = 0; i < size - 1; ++i) {
+        for (int j = 0; j < size - i - 1; ++j) {
+            if (arr[j] > arr[j + 1]) {
+                // Intercambiar los elementos
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+
+void insertionSort(int arr[], int size) {
+    for (int i = 1; i < size; ++i) {
+        int key = arr[i];
+        int j = i - 1;
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+
+void selectionSort(int arr[], int size) {
+    for (int i = 0; i < size - 1; ++i) {
+        int minIndex = i;
+        for (int j = i + 1; j < size; ++j) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+        swap(arr[i], arr[minIndex]);
+    }
+}
+
+
+
+void merge(int arr[], int left, int mid, int right) {
+    int i, j, k;
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+
+    int L[n1], R[n2];
+
+    for (i = 0; i < n1; ++i)
+        L[i] = arr[left + i];
+    for (j = 0; j < n2; ++j)
+        R[j] = arr[mid + 1 + j];
+    i = 0;
+    j = 0;
+    k = left;
+
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            ++i;
+        } else {
+            arr[k] = R[j];
+            ++j;
+        }
+        ++k;
+    }
+    while (i < n1) {
+        arr[k] = L[i];
+        ++i;
+        ++k;
+    }
+    while (j < n2) {
+        arr[k] = R[j];
+        ++j;
+        ++k;
+    }
+}
+
+void mergeSort(int arr[], int left, int right) {
+    if (left < right) {
+        int mid = left + (right - left) / 2;
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+        merge(arr, left, mid, right);
+    }
+}
+
+
+int partition(int arr[], int low, int high) {
+    int pivot = arr[high];
+    int i = low - 1;
+
+    for (int j = low; j < high; ++j) {
+        if (arr[j] < pivot) {
+            ++i;
+            swap(arr[i], arr[j]);
+        }
+    }
+    swap(arr[i + 1], arr[high]);
+    return i + 1;
+}
+
+
+void quickSort(int arr[], int low, int high) {
+    if (low < high) {
+        int pi = partition(arr, low, high);
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+
+void heapify(int arr[], int size, int i) {
+    int largest = i;
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
+
+    if (left < size && arr[left] > arr[largest]) {
+        largest = left;
+    }
+    if (right < size && arr[right] > arr[largest]) {
+        largest = right;
+    }
+    if (largest != i) {
+        swap(arr[i], arr[largest]);
+        heapify(arr, size, largest);
+    }
+}
+
+void heapSort(int arr[], int size) {
+    for (int i = size / 2 - 1; i >= 0; --i) {
+        heapify(arr, size, i);
+    }
+    for (int i = size - 1; i >= 0; --i) {
+        swap(arr[0], arr[i]);
+        heapify(arr, i, 0);
+    }
+}
